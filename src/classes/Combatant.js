dcaeeff5b1a540ce16b75f5e349c57848275a7fa
Mike van der Zijden <mike@vo-digitaal.nl>
2020-06-20 16:45:55 +0200

    fix copy function

--- src/classes/Combatant.js
+++ src/classes/Combatant.js
@@ -102,7 +102,7 @@ export class Combatant {
     }
 
     copy() {
-        let n = _.cloneDeep(this);
+        let n = this.clone();
 
         n.name = 'Copy of ' + n.name;
         n.id = getId();
